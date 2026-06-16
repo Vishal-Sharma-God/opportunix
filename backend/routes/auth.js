26fd81d0ef2419cad778ff8089d9b1ead868f04a
Vishal Sharma <nakulvishalsharma100@gmail.com>
2026-06-17 03:19:55 +0530

    Fix delete account User import

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,3 +1,4 @@
+const User = require('../models/User')
 const express = require('express')
 const router = express.Router()
 const { register, login, getMe, forgotPassword } = require('../controllers/authController')
